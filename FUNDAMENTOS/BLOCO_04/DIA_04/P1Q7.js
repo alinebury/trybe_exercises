05aa2d9dc2535a170eba6ed98674dcd176d0a609
alinebury@gmail.com <alinebury@gmail.com>
2021-12-01 18:24:19 -0300

    Exercicio7 Parte1/Bloco 4/Dia4

---
+++ FUNDAMENTOS/BLOCO_04/DIA_04/P1Q7.js
@@ -0,0 +1,16 @@
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [{
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+    }, ],
+};
+
+leitor.livrosFavoritos.push({
+    titulo: "Harry Potter e o Prisioneiro de Azkaban",
+    autor: "JK Rowling",
+    editor: "Rocco",
+})
