60dac393b1922d883ea8c17cfa53ed95f25d6e6e
alinebury@gmail.com <alinebury@gmail.com>
2021-12-01 11:15:08 -0300

    Exercicio 7/Dia 1/Bloco 4

---
+++ FUNDAMENTOS/BLOCO_04/DIA_01/Q7.js
@@ -0,0 +1,15 @@
+const nota = 90;
+
+if (nota >= 90) {
+    console.log("A");
+} else if (nota >= 80 && nota < 90) {
+    console.log("B");
+} else if (nota >= 70 && nota < 80) {
+    console.log("c");
+} else if (nota >= 60 && nota < 70) {
+    console.log("D");
+} else if (nota >= 50 && nota < 60) {
+    console.log("E");
+} else {
+    console.log("F");
+}
