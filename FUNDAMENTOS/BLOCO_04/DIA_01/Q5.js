49fc115134edb4089e332e400b73b4c855b16d65
alinebury@gmail.com <alinebury@gmail.com>
2021-12-01 10:46:47 -0300

    Exercicio 5/Dia 1/Bloco 4

---
+++ FUNDAMENTOS/BLOCO_04/DIA_01/Q5.js
@@ -0,0 +1,13 @@
+let angleA = 50;
+let angleB = 60;
+let angleC = 70;
+
+if (angleA > 0 && angleB > 0 && angleC > 0) {
+    if (angleA + angleB + angleC === 180) {
+        console.log("True");
+    } else {
+        console.log("False");
+    }
+} else {
+    console.log("Error!");
+}
