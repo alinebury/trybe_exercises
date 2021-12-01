41b58b817af522ec1d2357bd49a190dc6a476612
alinebury@gmail.com <alinebury@gmail.com>
2021-12-01 18:05:11 -0300

    Exercicio2 Parte1/Bloco 4/Dia4

--- FUNDAMENTOS/BLOCO_04/DIA_04/P1Q2.js
+++ FUNDAMENTOS/BLOCO_04/DIA_04/P1Q2.js
@@ -2,7 +2,8 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrent: "Sim",
 };
+
+info.recorrent = "Sim";
 
 console.log(info);
