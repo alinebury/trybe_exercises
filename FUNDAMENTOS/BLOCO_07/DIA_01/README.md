ccb2dd45d244ed0354de05ec1aa3b43b0d6e2615
Aline Bury <alinebury@gmail.com>
2022-01-12 16:27:14 -0300

    Exercicios Parte 1/Bloco 7/Dia 1

---
+++ FUNDAMENTOS/BLOCO_07/DIA_01/README.md
@@ -0,0 +1,8 @@
+##### Parte 1.
+1. Modificações necessárias na função para que seu comportamento respeite o escopo no qual cada variável foi declarada.
+    - Modifique para que seja uma 'arrow function'.
+    - Modifique as concatenações para 'template literals'.
+2. Faça com que a função retorne  o array em ordem crescente.
+    - Utilize 'template literals'.
+    - Fazendo o mesmo exercício utilizando o método array.sort().
+    
---
+++ FUNDAMENTOS/BLOCO_07/DIA_01/script.js
@@ -0,0 +1,44 @@
+function testingScope(escopo) {
+    if (escopo === true) {
+    //   var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+        ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+        console.log(ifScope);
+    } else {
+    //   var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        console.log(elseScope);
+    }
+    // o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  }
+
+testingScope(true);
+
+const arrowTestingScope = (escopo) => {
+    if(escopo === true){
+        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+        ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+        console.log(ifScope);
+    } else {
+        const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        console.log(elseScope);
+    }
+}
+arrowTestingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const returnArrayOrd = (num) =>{
+    for (let i=1 ; i<num.length; i++) {
+        for (let j=0 ; j<i ; j++) {  
+            if (num[i] < num[j]) {
+                let position = num[i];
+                num[i] = num[j];
+                num[j] = position;
+            }
+        }  
+    }
+    return num;
+}
+const sortedArray = returnArrayOrd(oddsAndEvens);
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
