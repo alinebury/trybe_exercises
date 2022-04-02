b1995deb65e5e36e79424d53cabeb4040a0d402c
Aline Bury <alinebury@gmail.com>
2022-04-02 14:29:50 -0300

    Exercicio 1 completo e 2 incompleto

--- FRONT-END/BLOCO_15_GERENCIAMENTO_DE_ESTADO_COM_REDUX/dia-02-usando-o-redux-no-react/exercise-react-with-redux-intro/src/store/index.js
+++ FRONT-END/BLOCO_15_GERENCIAMENTO_DE_ESTADO_COM_REDUX/dia-02-usando-o-redux-no-react/exercise-react-with-redux-intro/src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from '../reducers';
+// import rootReducer from '../reducers';
 import trafficSignalReducer from '../reducers/trafficSignalReducer';
 
 
