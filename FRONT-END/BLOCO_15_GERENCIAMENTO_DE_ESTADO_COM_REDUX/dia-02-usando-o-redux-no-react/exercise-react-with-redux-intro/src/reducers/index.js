7fe33acc8de014c3e00548f6c555cd1b7de151ab
Aline Bury <alinebury@gmail.com>
2022-04-02 14:25:21 -0300

    Revert "Bloco 15"
    This reverts commit f309518e0d3ff5dd9b45406f9430409e0d9bb10d.

--- FRONT-END/BLOCO_15_GERENCIAMENTO_DE_ESTADO_COM_REDUX/dia-02-usando-o-redux-no-react/exercise-react-with-redux-intro/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import trafficSignalReducer from './trafficSignalReducer';
-
-const rootReducer = combineReducers({ 
-  signal: trafficSignalReducer,
-});
-
-export default rootReducer;
